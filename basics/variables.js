f7c1450899ffff352b85b15d274518fd99aba8fe
Robert Elias <RobertElias@users.noreply.github.com>
2019-05-08 21:00:42 -0700

    variables

---
+++ basics/variables.js
@@ -0,0 +1,17 @@
+// cant define variables more than once
+
+let petName = 'Mommas'
+petName = 'Spot'
+
+
+console.log(petName)
+
+// there are rules related to variable names
+
+let test$ = 2
+let result = test$ + 3 * 4
+console.log(result)
+// 3. variable names cannot be reserved keywords
+
+
+
