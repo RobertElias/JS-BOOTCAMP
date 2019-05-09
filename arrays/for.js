374d7d720c8c61e22e9fbcf91e507ccbbd1a9b7f
Robert Elias <RobertElias@users.noreply.github.com>
2019-05-08 21:18:32 -0700

    The For Array

---
+++ arrays/for.js
@@ -0,0 +1,29 @@
+// for (let i = 0; i < 9; i++) {
+//     console.log(i);
+//     // more statements
+//  }
+
+// var i = 0;
+// for (; i < 9; i++) {
+//     console.log(i);
+//     // more statements
+// }
+
+
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+    //break;
+}
+for (let i = 0;; i++) {
+    console.log(i);
+    if (i > 3) break;
+    // more statements
+ }
+
+ var i = 0;
+
+for (;;) {
+  if (i > 3) break;
+  console.log(i);
+  i++;
+}
