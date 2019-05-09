19ce66d182521278932b2d48a102975445cd0aca
Robert Elias <RobertElias@users.noreply.github.com>
2019-05-08 21:16:32 -0700

    Undefined-Nulls

---
+++ functions/undefined-null.js
@@ -0,0 +1,25 @@
+// Undefined for variable
+let name 
+
+if (name === undefined) {
+    console.log('Please provide a name')
+} else {
+    console.log(name)
+}
+
+///Undefined for function arguments
+//Undefiined as function return default value
+
+let square = function (num) {
+    console.log(num)    
+}
+
+let result = square()
+console.log(result)
+
+
+//null as assigned value
+let age = 27
+age = null
+console.log(age)
+
