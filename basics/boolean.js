0b1eae668affbd0fb070abb9975cc06d94f99671
Robert Elias <RobertElias@users.noreply.github.com>
2019-05-08 20:57:03 -0700

    boolean

---
+++ basics/boolean.js
@@ -0,0 +1,43 @@
+// === -equality operator
+//!== -not equal operator
+// < -less than operator
+// > -greater than operator
+// <= -less than or equial to operator
+// >= -greater than or equal to operator
+
+let temp = 180
+//let isFreezing = temp <= 32
+
+if (temp <= 32) {    
+    console.log('It is freesing outside!') 
+}
+
+if (temp >= 110) {
+    console.log('It is way to hot outside!')
+    console.log("Testing 123!!!")
+}
+
+
+
+
+
+//Challenge
+
+// Create age variable to your age
+let age = 65
+
+
+// if 7 or under print message about child pricing
+
+if (age <=7) {
+    console.log("You get childrens disount!!!")
+}
+
+// if 65 or older print message about senior discount
+if (age >= 65) {
+    console.log('You get the Senior Discount!')
+}
+
+
+    
+
