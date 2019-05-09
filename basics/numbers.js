a846a123a4ae9963aa44e65e437b1803835ddc1d
Robert Elias <RobertElias@users.noreply.github.com>
2019-05-08 20:56:10 -0700

    numbers

---
+++ basics/numbers.js
@@ -0,0 +1,17 @@
+let age = 42
+let dogYears = (age + 1) / 7
+
+console.log(dogYears) // prints dogYears
+
+// Challenge Area
+//studentScore 18
+//maxScore 20
+//percent 90
+
+//print the percent
+
+
+let studentScore = 41
+let maxScore = 100
+let percent = (studentScore / maxScore) * 100
+console.log(percent)
