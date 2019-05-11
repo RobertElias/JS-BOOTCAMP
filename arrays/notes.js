aa99aaaea8a6c65bba6908834cff3da987776610
Robert Elias <RobertElias@users.noreply.github.com>
2019-05-11 16:17:30 -0700

    updates

--- arrays/notes.js
+++ arrays/notes.js
@@ -47,14 +47,23 @@ const notes = [{
 }]
 
 
+const sortNotes = function(notes) {
+    notes.sort(function(a, b){
+        if(a.title.toLowerCase() < b.title.toLowerCase()) {
+            return - 1
+        } else if (b.title.toLowerCase() < a.title.toLowerCase()){
+            return 1
+        } else {
+            return 0
+        }
+    })
+}
 const findNote = function(notes, noteTitle) {
     return notes.find(function(note, index) {
     return note.title.toLowerCase() === noteTitle.toLowerCase() 
     })
     
 }
-
-
 const findNotes = function (notes, query) {
     return notes.filter(function (note, index){
         const isTitleMatch = note.title.toLowerCase().includes(query.toLowerCase)
@@ -67,12 +76,12 @@ const findNotes = function (notes, query) {
 
 
 
-console.log(findNotes(notes, 'work'))
+// console.log(findNotes(notes, 'work'))
 
 
 
-const note = findNote(notes, 'some other office modification')
-console.log(note)
+// const note = findNote(notes, 'some other office modification')
+// console.log(note)
 
 // const note = findNote(notes, 'office modification')
 // console.log(note)
@@ -97,3 +106,5 @@ console.log(note)
 
 //--44 Filtering Arrays
 
+sortNotes(notes)
+console.log(notes)
