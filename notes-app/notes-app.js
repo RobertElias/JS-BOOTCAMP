66bd55831c6fef4f3823b16cd3460bd92e6afea0
Robert Elias <RobertElias@users.noreply.github.com>
2019-05-18 21:57:08 -0700

    Updates

---
+++ notes-app/notes-app.js
@@ -0,0 +1,15 @@
+//DOM Document Object Model
+
+// //Query and remove
+// const p = document.querySelector('p');
+
+// p.remove();
+
+//Query all and remove
+
+const ps = document.querySelectorAll('p')
+
+ps.forEach(function (p) {
+    //p.remove()
+    console.log(p.textContent)
+})
