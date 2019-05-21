73e4d342a11b16ca8d09f9a653ebd0719dc18b42
Robert Elias <RobertElias@users.noreply.github.com>
2019-05-20 21:50:44 -0700

    Updates

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,3 +1,15 @@
+
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain.'
+}, {
+    title: 'Habbits to work on ',
+    body: 'Exercise. Eating a bit healthier.'
+},{
+    title: 'Office modification',
+    body: 'Get a new seat.'
+}]
+
 //DOM Document Object Model
 
 // //Query and remove
@@ -22,3 +34,4 @@ const newParagraph = document.createElement('p')
 
 newParagraph.textContent = 'This is a new element from JavaScript'
 document.querySelector('body').appendChild(newParagraph)
+
