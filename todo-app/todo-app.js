5104bdeb7d327034cde6e5e756836ad5d468c524
Robert Elias <RobertElias@users.noreply.github.com>
2019-05-20 21:56:13 -0700

    Updated for each loop

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -27,7 +27,11 @@ const summary = document.createElement('h2')
 summary.textContent = `You have ${incompleteTodos.length} todos left.`
 document.querySelector('body').appendChild(summary)
 
-
+todos.forEach(function(todo) {
+    const p = document.createElement('p')
+    p.textContent = todo.text
+    document.querySelector('body').appendChild(p)    
+})
 // const paragraphs = document.querySelectorAll('p')
 
 // paragraphs.forEach(function (paragraph) {
