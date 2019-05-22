6ac802fc5640e0953f95495b21b2c53d253fbc3d
Robert Elias <RobertElias@users.noreply.github.com>
2019-05-21 22:04:08 -0700

    Updates

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -10,10 +10,10 @@ const notes = [{
     body: 'Get a new seat.'
 }]
 
-document.querySelector('button').addEventListener('click', function (e){
-    e.target.textContent = "The button was clicked"
+document.querySelector('#create-note').addEventListener('click', function (e) {
+    e.target.textContent = 'The button was clicked'
 })
 
-document.querySelectorAll('button')[1].addEventListener('click', function (){
-    console.log("Delete all notes")
+document.querySelector('#remove-all').addEventListener('click', function () {
+    console.log('Delete all notes')
 })
