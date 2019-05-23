1df4d82b3fdf582794fee546461bb45aaad71649
Robert Elias <RobertElias@users.noreply.github.com>
2019-05-22 21:00:37 -0700

    Updates

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,13 +1,12 @@
-
 const notes = [{
-    title: 'My next trip',
-    body: 'I would like to go to Spain.'
+    title: 'my next trip',
+    body: 'I would like to go to Spain'
 }, {
-    title: 'Habbits to work on ',
-    body: 'Exercise. Eating a bit healthier.'
-},{
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better.'
+}, {
     title: 'Office modification',
-    body: 'Get a new seat.'
+    body: 'Get a new seat'
 }]
 
 const filters = {
@@ -16,27 +15,29 @@ const filters = {
 
 const renderNotes = function (notes, filters) {
     const filteredNotes = notes.filter(function (note) {
-        return note.title.toLowerCase().includes(filters.searchText.totalLowerCase)
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
     })
+
+    document.querySelector('#notes').innerHTML = ''
     
-    filteredNotes.forEach(function(note) {
+    filteredNotes.forEach(function (note) {
         const noteEl = document.createElement('p')
         noteEl.textContent = note.title
-        document.querySelector('body').appendChild(noteEl)
+        document.querySelector('#notes').appendChild(noteEl)
     })
 }
 
 renderNotes(notes, filters)
+
 document.querySelector('#create-note').addEventListener('click', function (e) {
     e.target.textContent = 'The button was clicked'
 })
 
 document.querySelector('#remove-all').addEventListener('click', function () {
-    document.querySelectorAll('.note').forEach(function(note) {
+    document.querySelectorAll('.note').forEach(function( note) {
         note.remove()
     })
 })
-
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
     filters.searchText = e.target.value
