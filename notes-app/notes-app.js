d8e768d730aeb2028d9f698586021c761ff3ae6e
Robert Elias <RobertElias@users.noreply.github.com>
2019-05-22 20:48:11 -0700

    Updates

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -10,6 +10,23 @@ const notes = [{
     body: 'Get a new seat.'
 }]
 
+const filters = {
+    searchText: ''
+}
+
+const renderNotes = function (notes, filters) {
+    const filteredNotes = notes.filter(function (note) {
+        return note.title.toLowerCase().includes(filters.searchText.totalLowerCase)
+    })
+    
+    filteredNotes.forEach(function(note) {
+        const noteEl = document.createElement('p')
+        noteEl.textContent = note.title
+        document.querySelector('body').appendChild(noteEl)
+    })
+}
+
+renderNotes(notes, filters)
 document.querySelector('#create-note').addEventListener('click', function (e) {
     e.target.textContent = 'The button was clicked'
 })
@@ -22,7 +39,8 @@ document.querySelector('#remove-all').addEventListener('click', function () {
 
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
-    console.log(e.target.value)
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
 })
 
 //--Single--
