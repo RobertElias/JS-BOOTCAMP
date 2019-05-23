14796bf4b72181e76c1071e415af0c7d02992fed
Robert Elias <RobertElias@users.noreply.github.com>
2019-05-22 20:37:16 -0700

    Updates

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -44,3 +44,7 @@ document.querySelector('#add-todo').addEventListener('click', function (e){
     console.log('Add a new todo...')
   
 })
+
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    console.log(e.target.value)
+})
