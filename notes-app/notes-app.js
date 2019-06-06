a35d96ff43669fb41756bb5ae4dbe909ad59fa81
Robert Elias <RobertElias@users.noreply.github.com>
2019-06-05 20:07:57 -0700

    Updated Forms

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -41,6 +41,7 @@ document.querySelector('#search-text').addEventListener('input', function (e) {
 document.querySelector('#name-form').addEventListener('submit', function (e) {
     e.preventDefault()
     console.log(e.target.elements.firstName.value)
+    e.target.elements.firstName.value = ''
 })
 
 //--Single--
