bdf1005d5016571f7f8a4a64a70418cd97ca8af9
Robert Elias <RobertElias@users.noreply.github.com>
2019-06-06 19:55:49 -0700

    JSON Updates

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -18,7 +18,20 @@ const filters = {
 //console.log(localStorage.getItem('location'))
 //localStorage.removeItem('location')
 
-localStorage.clear()
+// localStorage.clear()
+
+// const user = {
+//     name:"Robert",
+//     age: 27
+// }
+
+// const userJSON = JSON.stringify(user)
+// console.log(userJSON)
+// localStorage.setItem('user', userJSON)
+
+const userJSON = localStorage.getItem('user')
+const user = JSON.parse(userJSON)
+console.log(`${user.name} is ${user.age}`)
 
 const renderNotes = function (notes, filters) {
     const filteredNotes = notes.filter(function (note) {
