cc937d075c1890bc31e52f1e2ab276ef1ae67c80
Robert Elias <RobertElias@users.noreply.github.com>
2019-06-06 19:49:09 -0700

    Local Storage Updates

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -13,6 +13,13 @@ const filters = {
     searchText: ''
 }
 
+//localStorage.setItem('location', 'Orange Country')
+
+//console.log(localStorage.getItem('location'))
+//localStorage.removeItem('location')
+
+localStorage.clear()
+
 const renderNotes = function (notes, filters) {
     const filteredNotes = notes.filter(function (note) {
         return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
